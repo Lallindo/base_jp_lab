730be4c73fe0f657201c9b9f5930af3e8993e936
Bruno Pavan Silva Lallo <brunolallo8@gmail.com>
2025-02-10 12:20:53 -0300

    Alteração nas dependências

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ description = 'Pacote com objetos que serão utilizados para criação de sistem
 
 setup(
     name='base_jp_lab',
-    version='0.0.1',
+    version='0.0.2',
     description=description,
     author='Bruno Lallo',
     author_email='brunolallo8@gmail.com',
     packages=find_packages(),
     install_requires=[
-        "requests==2.32.3", "selenium==4.24.0", "gspread==6.1.4", "oauth2client==4.1.3", "pandas==2.2.3"
+        "requests==2.32.3", "selenium==4.24.0", "gspread==6.1.4", "oauth2client==4.1.3", "pandas==2.2.3", "mysql-connector==2.2.9"
         ],
     license='MIT'
 )
