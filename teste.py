1ea82ed0c97fab1d9c24010b2284aa17d29fcc38
Bruno Pavan Silva Lallo <124905893+Lallindo@users.noreply.github.com>
2025-02-11 12:12:37 -0300

    Delete teste.py

--- teste.py
+++
@@ -1,5 +0,0 @@
-from base_jp_lab.Objects import Access, decrypt_msg
-
-a = Access('root', '', 'localhost', '3306', 'jp_bd')
-r = a.custom_select_query('SELECT * FROM apis WHERE id_api = 5')[0]
-print(decrypt_msg(r[8], 'amazon'))
