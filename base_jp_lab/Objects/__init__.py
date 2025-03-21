a26ee868d49a55f6f5d41ec9f71cee3f5fd9a7aa
Lallindo <brunolallo8@gmail.com>
2025-03-21 11:12:01 -0300

    Correção

--- base_jp_lab/Objects/__init__.py
+++ base_jp_lab/Objects/__init__.py
@@ -1,6 +1,6 @@
 from .GetData import api_data, api_token_n8n, scraping_base_url, sheet_data, alter_sheet, set_api_keys, encrypt_msg, decrypt_msg
 from .ConnectionClass import Connection
 from .AccessClass import Access
+from .AsyncCallerClass import AsyncCaller
 from .CallerClass import Caller
 from .ScraperClass import Scraper
-from .AsyncCallerClass import AsyncCaller
